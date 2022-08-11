f8fa0d6fed23b323a35bbb63cfa68c3a475c18a0
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2022-08-11 15:33:54 +0500

    create a static website

---
+++ expressStaticWebsite.js
@@ -0,0 +1,7 @@
+// const express = require('express')
+// const path = require('path')
+
+// const app = express()
+// const publicDirPath = path.join(__dirname, 'public')
+
+// app.use(express.static(publicDirPath)).listen(7000)
