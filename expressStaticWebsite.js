134030f3c80d6cdf040be4fa67d62fbf735f2c8e
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2022-08-11 15:44:46 +0500

    remove .html extension

--- expressStaticWebsite.js
+++ expressStaticWebsite.js
@@ -4,4 +4,18 @@ const path = require('path')
 const app = express()
 const publicDirPath = path.join(__dirname, 'public')
 
-app.use(express.static(publicDirPath)).listen(7000)
+//--------- show page with .html extension-----------------
+// app.use(express.static(publicDirPath)).listen(7000)
+
+
+//--------- remove .html extension-----------------
+
+app.get('', (req, res) =>{
+    res.sendFile(`${publicDirPath}/index.html`)
+})
+
+app.get('/about', (req, res) =>{
+    res.sendFile(`${publicDirPath}/about.html`)
+})
+
+app.listen(7000)
