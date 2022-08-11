074eaaac0ac2e0bf126cb67b45dff16c8dcbbad1
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2022-08-11 15:34:13 +0500

    Update expressStaticWebsite.js

--- expressStaticWebsite.js
+++ expressStaticWebsite.js
@@ -1,7 +1,7 @@
-// const express = require('express')
-// const path = require('path')
+const express = require('express')
+const path = require('path')
 
-// const app = express()
-// const publicDirPath = path.join(__dirname, 'public')
+const app = express()
+const publicDirPath = path.join(__dirname, 'public')
 
-// app.use(express.static(publicDirPath)).listen(7000)
+app.use(express.static(publicDirPath)).listen(7000)
