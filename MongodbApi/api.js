420c701f262bb71b8584be878c809f3fce471866
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2022-08-23 12:37:19 +0500

    create user post api

---
+++ MongodbApi/api.js
@@ -0,0 +1,30 @@
+const dbConnect = require("../connectMongodb");
+const express = require("express");
+const { application } = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const db = await dbConnect();
+  const userCollection = db.collection("user");
+  const userData = await userCollection.find().toArray();
+  console.log("userData", userData.length);
+  res.send(userData);
+});
+
+app.post("/createUser", async (req, res) => {
+  const userData = req.body;
+  console.log("userData", userData);
+  const db = await dbConnect();
+  const userCollection = db.collection("user");
+  const newUser = await userCollection.insert(userData);
+  if (newUser.acknowledged) {
+    res.send("User " + userData.first_name + " Successfully Created");
+  }
+});
+
+app.listen(4000, () =>
+  console.log("Server is runing on port: http://localhost:4000")
+);
