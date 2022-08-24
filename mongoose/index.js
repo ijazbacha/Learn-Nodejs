b85be3054af8e0da6f168dad8b14b0d2e357d2b0
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2022-08-24 15:41:21 +0500

    search API with mongoose

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,32 +1,46 @@
-const { application } = require('express');
-const express = require('express')
-require('./config')
-const User = require('./user')
+const { application } = require("express");
+const express = require("express");
+require("./config");
+const User = require("./user");
 
-const app = express()
+const app = express();
 app.use(express.json());
 
-app.get('/', async (req, res) =>{
-    let result = await User.find()
-    console.log('result', result);
-    res.send(result)
-})
+app.get("/", async (req, res) => {
+  let result = await User.find();
+  console.log("result", result);
+  res.send(result);
+});
 
-app.post('/create', async (req, res) =>{
-    const user = new User(req.body)
-    const result = await user.save()
-    res.send(result)
-})
+app.post("/create", async (req, res) => {
+  const user = new User(req.body);
+  const result = await user.save();
+  res.send(result);
+});
 
-app.put('/update/:_id',async (req, res) =>{
-    const user = await User.updateOne(req.params, {$set: req.body})
-    console.log('user', user);
-    res.send(user)
-})
+app.put("/update/:_id", async (req, res) => {
+  const user = await User.updateOne(req.params, { $set: req.body });
+  console.log("user", user);
+  res.send(user);
+});
 
-app.delete('/delete/:_id',async (req, res) =>{
-    const result = await User.deleteOne(req.params)
-    res.send(result)
-})
+app.delete("/delete/:_id", async (req, res) => {
+  const result = await User.deleteOne(req.params);
+  res.send(result);
+});
 
-app.listen(5000)
+app.get("/search/:query", async (req, res) => {
+  const result = await User.find({
+    $or: [
+      // -----------singal field search------------
+      //    {"first_name": {$regex: req.params.query}}
+
+      //----------multiple fileds search-----------
+      { first_name: { $regex: req.params.query } },
+      { last_name: { $regex: req.params.query } },
+    ],
+  });
+  res.send(result);
+});
+
+app.listen(5000);
