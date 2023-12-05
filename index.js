3cd10c0ddca3fdee19e59475e73e06656938aa92
Ijaz Bacha <ijazbacha14@gmail.com>
2023-12-05 20:48:21 +0500

    create a server with express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Start leating");
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) =>{
+    res.send("This the begning")
+})
+
+app.listen(port, () =>{
+    console.log("Server is running on this port", port);
+})
